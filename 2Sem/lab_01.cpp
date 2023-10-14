bf4ccf2d1f7f1dac195dd0d7a7f72e7d6bfba6b6
n0tmyself <alshevskiy.dima@mail.ru>
2023-10-14 08:51:29 +0300

    add lab_01

---
+++ 2Sem/lab_01.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include<string>
+#include <vector>
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+void BubbleSort(std::vector<int>& values)
+{
+    for(size_t i = 0; i + 1 < values.size(); ++i)
+    {
+        for(size_t j = 0; j + 1 < values.size() - i; ++j)
+        {
+            if(values[j + 1] < values[j])
+            {
+                std::swap(values[j + 1], values[j]);
+            }
+        }
+    }
+}
+
+int partition(std::vector<int>& values, int l, int r)
+{
+    int a = (l + r) / 2;
+    int v = values[a];
+    int i = l;
+    int j = r;
+    while(i <= j)
+    {
+        while(values[i] <  v)
+        {
+            ++i;
+        }
+        while(values[j] > v)
+        {
+            --j;
+        }
+        if (i >= j){
+            break;
+        }
+        std::swap(values[i], values[j]);
+        ++i;
+        --j;
+    }
+    return j;   
+}
+
+void QuickSort(std::vector<int>& values, int l, int r)
+{
+    // int l = 0;
+    // int r = values.size() - 1;
+    if (l < r)
+    {
+        int p = partition(values, l, r);
+        QuickSort(values, p + 1, r);
+        QuickSort(values, l, p);
+    }
+}
+
+
+
+int main()
+{
+    int size = 10;
+
+
+    // std::vector<int> vec = {5, 0, 1, 6, 3, 100 , 3, 40, 41, 40, 3, 6};
+
+    std::vector<int> vec(size, 0);
+    srand(time(0));
+    generate(vec.begin(), vec.end(), rand);
+
+    std::cout << "Random: ";
+    for(size_t i = 0; i < vec.size(); ++i)
+    {
+        std::cout << vec[i] << ' ';
+    } 
+    std::cout << '\n';
+
+    // BubbleSort(vec);
+    QuickSort(vec, 0, vec.size() - 1);
+    std::cout << "Sorted: ";
+    for(size_t i = 0; i < vec.size(); ++i)
+    {
+        std::cout << vec[i] << ' ';
+    }    
+}
